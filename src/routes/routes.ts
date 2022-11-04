ba2c9236dc744023f9515a3774d44a3909ca452b
Esteban Puentes <esteban.puentes@bewe.io>
2022-11-04 12:32:25 -0500

    adding nested chunck to lazyload a dashboard per say with all its children components

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import {lazy, LazyExoticComponent} from "react";
+import { NoLazy } from "../01-lazyLoad/pages/NoLazy";
 // import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyLoad/pages";
 
 type JSXComponent = ()=> JSX.Element
@@ -10,28 +11,26 @@ interface Route{
     Component: LazyExoticComponent<JSXComponent> | JSXComponent
 }
 
-const Lazy1 = lazy(()=> import(/* webpackChunkName: "LazyPage1" */ "../01-lazyLoad/pages/LazyPage1") )
-const Lazy2 = lazy(()=> import(/* webpackChunkName: "LazyPage2" */ "../01-lazyLoad/pages/LazyPage2") )
-const Lazy3 = lazy(()=> import(/* webpackChunkName: "LazyPage3" */ "../01-lazyLoad/pages/LazyPage3") )
+const LazyLayout = lazy(()=> import(/* webpackChunkName: "LazyLayoutPage1" */ "../01-lazyLoad/layout/LazyLayout") )
 
 
 export const routes : Route[] = [
     {
-        to: "/lazy1",
-        path: "lazy1",
-        Component:Lazy1,
-        name: "Lazy-1"
+        path: "/lazyload/*",
+        to: "/lazyload/",
+        Component:LazyLayout,
+        name: "Lazy Layout"
     },
     {
-        to: "/lazy2",
-        path: "lazy2",
-        Component:Lazy2,
-        name: "Lazy-2"
+        to: "/no-lazy",
+        path: "no-lazy",
+        Component:NoLazy,
+        name: "No lazy"
     },
-    {
-        to: "/lazy3",
-        path: "lazy3",
-        Component:Lazy3,
-        name: "Lazy-3"
-    }
+    // {
+    //     to: "/lazy3",
+    //     path: "lazy3",
+    //     Component:Lazy3,
+    //     name: "Lazy-3"
+    // }
 ]
