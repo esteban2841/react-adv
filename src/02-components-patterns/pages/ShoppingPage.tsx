a856754eef40dc475cc709e26bbeff174985e8d7
Esteban Puentes <esteban.puentes@bewe.io>
2022-11-16 21:26:09 -0500

    pre-npm ready

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
 import { products } from '../data/products';
-import "../styles/custom-styles.css";
 
 
 export const ShoppingPage = () => {
@@ -16,7 +15,6 @@ export const ShoppingPage = () => {
         <ProductCard 
           key={product.id}
           product={product}
-          className='bg-dark' 
           initialValues={{
             count:4,
             maxCount:10
@@ -27,14 +25,10 @@ export const ShoppingPage = () => {
               return (
 
               <>
-                <ProductImage className='custom-image'/>
-                <ProductTitle  className={"color-white"}/>
-                <ProductButtons className='custom-buttons' />
+                <ProductImage />
+                <ProductTitle  />
+                <ProductButtons />
 
-                <button onClick={reset}>Reset</button>
-                {!isMaxCountReached ? <button onClick={()=>increaseBy(-2)}>-2</button>:null}
-                {!isMaxCountReached ? <button onClick={()=>increaseBy(+2)}>+2</button>:null}
-                <span>{count}</span>
               </> 
               )
             }
